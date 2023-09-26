9efeee3044bf10156db0c40d4414e630420e99d8
Tuna <Tdb00096@gmail.com>
2023-09-26 17:48:36 -0400

    de

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,6 @@
 import express, { json } from 'express';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 443;
 import cors from 'cors';
 import tables from './db/tables.js';
 import router from './routes/route.js';
