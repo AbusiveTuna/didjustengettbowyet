8aca5ca03c45d069a5b82b78e103c2ebca1f06d8
Tuna <Tdb00096@gmail.com>
2023-09-26 17:36:08 -0400

    hardcoded

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -2,7 +2,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgresql://abusivetuna:test1233@coxbingodb.ccsjvwbnhbrj.us-east-1.rds.amazonaws.com:5432/abusivetuna",
   ssl: {
     rejectUnauthorized: false
   }
