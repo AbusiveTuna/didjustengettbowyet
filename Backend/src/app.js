63361c81f796275e6928699cc1172dd6bb7f40a3
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-09-27 12:48:44 +0000

    all

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,7 +23,7 @@ const credentials = {
 };
 
 app.use(cors({
-    origin: ['https://didjustengettbowyet.com', 'https://localhost:3000', 'https://stunning-happiness-rvwg96vgxqr2qw-3000.app.github.dev'],
+    origin: ['https://didjustengettbowyet.com', 'https://localhost:3000', '*'],
     optionSuccessStatus: 200
 }));
 
