eedba3b705c06ef4b84bb4db8f0df7071602e1da
Tuna <Tdb00096@gmail.com>
2023-09-28 16:00:23 -0400

    this makes sense to me but we will see tomorrow

--- Frontend/src/components/KC.js
+++ Frontend/src/components/KC.js
@@ -1,41 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const fetchData = (url, setFunction, teamName) => {
+  axios.get(url)
+    .then(response => {
+      let data = response.data;
+     
+      if (data[teamName] !== undefined) {
+        setFunction(data[teamName]);
+      }
+      else if (data["Goose S Gringos"] && teamName == "Goose's Gringos") {
+        setFunction(data[teamName]);
+      }
+    })
+    .catch(error => {
+      console.log('An error occurred while fetching the data:', error);
+    });
+};
+
 const KC = ({ teamName }) => {
   const [kc, setKc] = useState(0);
   const [kcCM, setKcCM] = useState(0);
+
   useEffect(() => {
-    axios.get('https://osrscharterships.com:3000/templeData')
-      .then(response => {
-        let data = response.data;
-
-        // Check if our team name matches then display the xp_gained as KC
-        if (data[teamName]) {
-          setKc(data[teamName]);
-        }
-      })
-      .catch(error => {
-        console.log('An error occurred while fetching the data:', error);
-      });
-
-      axios.get('https://osrscharterships.com:3000/templeDataCM')
-      .then(response => {
-        let data = response.data;
-
-        // Check if our team name matches then display the xp_gained as KC
-        if (data[teamName]) {
-          setKcCM(data[teamName]);
-        }
-      })
-      .catch(error => {
-        console.log('An error occurred while fetching the data:', error);
-      });
+    fetchData('https://osrscharterships.com:3000/templeData', setKc, teamName);
+    fetchData('https://osrscharterships.com:3000/templeDataCM', setKcCM, teamName);
   }, [teamName]);
 
   return (
     <div>
       <p>CoX KC: {kc}</p>
-      <p>CM KC:  {kcCM}</p>
+      <p>CM KC: {kcCM}</p>
     </div>
   );
 };
