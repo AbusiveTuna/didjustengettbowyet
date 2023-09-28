5391dd5f24bb6ae928d5682042f0a8138498013b
Tuna <Tdb00096@gmail.com>
2023-09-28 16:12:45 -0400

    updating toolbar

--- Frontend/src/components/Toolbar.js
+++ Frontend/src/components/Toolbar.js
@@ -6,7 +6,7 @@ const Toolbar = () => {
       <a href="/">Did Justen Get His Tbow Yet?</a>
       <a href="/gooseCoxBingo">Goose's CoX "Bingo"</a>
       <a href="https://templeosrs.com/groups/overview.php?id=2176">Ironman CC Group Page</a>
-      <a href="https://templeosrs.com/groups/overview.php?id=2176">Temple OSRS Competition Page</a>
+      <a href="https://templeosrs.com/competitions/standings.php?id=23801">Temple OSRS Competition Page</a>
     </div>
   );
 };
