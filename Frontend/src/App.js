9f188a80720264b3886a7b8e81436538b7a09a89
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-10-02 12:43:32 +0000

    adding toa/tob

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import JustenMeme from './pages/JustenMeme';
 import CoxTracker from './pages/CoxTracker';
+import TobTracker from './pages/TobTracker';
+import ToaTracker from './pages/ToaTracker';
 import AdminPage from './pages/AdminPage';
 import Toolbar from './components/Toolbar';
 import SiteCredits from './components/SiteCredits';
@@ -15,6 +17,8 @@ function App() {
             <Routes>
             <Route path="/" element={<JustenMeme/>} />
             <Route path="/gooseCoxBingo" element={<CoxTracker/>} />
+            <Route path="/tob" element={<TobTracker/>} />
+            <Route path="/toa" element={<ToaTracker/>} />
             <Route path="/dontgivethisfuckingurlaway" element={<AdminPage/>} />
             </Routes>
             
