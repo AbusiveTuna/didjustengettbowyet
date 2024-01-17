5eb07b7ced122b7998cedaa8fa26f9c35840f665
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-01-17 11:46:19 +0000

    fixing image size

--- Frontend/src/pages/PreviousEvents.js
+++ Frontend/src/pages/PreviousEvents.js
@@ -7,7 +7,7 @@ const PreviousEvents = () => {
     return (
         <div className="PreviousEvents">
             <h2>In October of 2023 Goose's Gringos defeated MX and the Arcanes in a bloody war that saw both teams competing to see who could get all 12 purples from CoX first.</h2>
-            <img src={gooseCox} alt="Goose CoX Bingo 2023" />
+            <img src={gooseCox} alt="Goose CoX Bingo 2023" className="goose-cox-history-image" />
             <h3> Goose CoX Bingo 2023 </h3>
         </div>
     );
