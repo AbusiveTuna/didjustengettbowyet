cf0f7ff80b6cda66c562a40625194a7fdcb79352
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-01-19 08:54:30 -0500

    Update playersData.js

--- Frontend/src/utilities/playersData.js
+++ Frontend/src/utilities/playersData.js
@@ -17,7 +17,7 @@ const playersData = [
     { "name": "Goose World", "amount": 110 },
     { "name": "High Bonsai", "amount": 0 },
     { "name": "Imago Loop", "amount": 10 },
-    { "name": "Iron Chkn", "amount": 0 },
+    { "name": "Iron Chkn", "amount": 10 },
     { "name": "Ironcg", "amount": 20 },
     { "name": "J mes", "amount": 10 },
     { "name": "Joan Of Arf", "amount": 10 },
