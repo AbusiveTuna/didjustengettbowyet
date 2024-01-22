a5d4b338bb05e84b8fa8a2d6d6f087637b2db100
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-01-21 19:34:08 -0500

    Update playersData.js

--- Frontend/src/utilities/playersData.js
+++ Frontend/src/utilities/playersData.js
@@ -7,6 +7,7 @@ const playersData = [
     { "name": "ags friend", "amount": 10 },
     { "name": "bongij_mi", "amount": 10 },
     { "name": "Combatt1ron", "amount": 10 },
+    { "name": "Cuhmmies", "amount": 11 },
     { "name": "Connorz", "amount": 20 },
     { "name": "Desports Btw", "amount": 10 },
     { "name": "Don Corgione", "amount": 10 },
