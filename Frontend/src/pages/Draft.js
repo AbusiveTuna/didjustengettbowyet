8270abe24b675c7feddf046cd1289023f58d97ef
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-01-23 20:02:04 -0500

    Update Draft.js

--- Frontend/src/pages/Draft.js
+++ Frontend/src/pages/Draft.js
@@ -17,8 +17,8 @@ const StaticTeamContainer = ({ team, players }) => (
 );
 
 const Draft = () => {
-    const teamTunaPhishPlayers = ["AbusiveTunaa"];
-    const teamNsyncPlayers = ["a sync"];
+    const teamTunaPhishPlayers = ["Abrafebre","Abusivetunaa","bongij_mi","ChNPP","Cuhmmies","connorz","Desports Btw","Fe brimstone","Frelya","Goose L","IM Ivar","Iron Chkn","J_mes","Joan of Arf","Justen","MilkMate","MXIIA","Noble Mobile","I run East","Sailor Terry","Sacred Clays","KinuskiCorie","Shaharrav","Tiiiiiiiiiim","Vyraal1","Vorkathchan","Zero Rangers"];
+    const teamNsyncPlayers = ["A sqrd","Astraleos","Fatboislimrs","a sync","ags friend","bid whist","Combatt1ron","Don Corgione","dj i nn","F aladorable","F loofy","GM Goblin","kazpu","Kundaii","Goose World","High Bonsai","Imago Loop","Ironcg","Lord Oziach","Matt93","Nerding Out","I got no job","Ninef","P Dirty Soda","St_rkWolf","S 7efen","Slaymebb","sticky died"];
 
     return (
         <div className="draft">
