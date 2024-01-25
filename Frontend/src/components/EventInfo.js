4aa2d6fe1f4838f3a0d39d26834546eede55c412
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-01-24 19:21:44 -0500

    Update EventInfo.js

--- Frontend/src/components/EventInfo.js
+++ Frontend/src/components/EventInfo.js
@@ -16,9 +16,9 @@ const EventInfo = () => {
             <h2>Event Details</h2>
             <p>The raid completion event is a race to complete all three OSRS raids. Two teams will be drafted from the list of participating players. The first team to obtain all 26 items wins.</p>
             <p>Users will submit their drops via Discord, and once verified can see their drop updated on the tracker: <a href="/tunaBingo">Link to Tracker</a></p>
-            <p>Draft Date: {draftDate}</p>
-            <p>Event Start: {startDate}</p>
-            <p>Event End: {endDate}</p>
+            <p>Draft Date: whatever the discord says.</p>
+            <p>Event Start: whatever the discord says.</p>
+            <p>Event End: whatever the discord says.</p>
         </div>
     );
 };
