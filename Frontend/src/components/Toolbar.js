ce6791d6abfbecfa695b25a863c6c6992aabd1de
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-01-26 07:47:43 -0500

    Update Toolbar.js

--- Frontend/src/components/Toolbar.js
+++ Frontend/src/components/Toolbar.js
@@ -6,6 +6,7 @@ const Toolbar = () => {
       <a href="/draft">2024 Tuna/aSync Raid Completion Mega Draft</a>
       <a href="/eventDetails">Raid Completion Bingo Details</a>
       <a href="/tunaBingo">Raid Completion Bingo Tracker</a>
+      <a href="https://www.templeosrs.com/competitions/standings.php?id=24834">TempleOSRS Tracker</a>
       <a href="/previousEvents">Previous Events</a>
     </div>
   );
