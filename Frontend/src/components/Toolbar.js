462b5d22ed3267d6c798c38f4c3fa45dd5c7a95d
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-02-24 18:51:58 -0500

    Update Toolbar.js

--- Frontend/src/components/Toolbar.js
+++ Frontend/src/components/Toolbar.js
@@ -3,6 +3,8 @@ import './css/Toolbar.css';
 const Toolbar = () => {
   return (
     <div className="toolbar">
+      <a href="/">Did Justen Get His Tbow Yet?</a>
+      <a href="https://templeosrs.com/groups/overview.php?id=2176">Ironman CC Group Page</a>
       <a href="/previousEvents">Previous Events</a>
     </div>
   );
